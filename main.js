5230c117ec110a9e053afb23f6355a47bfbb31d7
Auro Mota <palubo01@gmail.com>
2016-05-10 20:25:45 -0300

    Initial commit.

---
+++ main.js
@@ -0,0 +1,20 @@
+var app = require('app');
+var BrowserWindow = require('browser-window');
+
+var mainWindow = null;
+
+app.on('window-all-closed', function() {
+    // if platform is Mac, ensure that only cmd + Q will quit
+    if(process.platform != 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('ready', function() {
+    mainWindow = new BrowserWindow({width: 500, height: 500});
+    mainWindow.setMenu(null);
+    mainWindow.loadURL('file://' + __dirname + '/view/index.html');
+    mainWindow.on('closed', function() {
+        mainWindow = null;
+    });
+});
